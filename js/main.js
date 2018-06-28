b116a6409819db761a5e43c125768b077a192d70
keylinD <keylin.05.01@gmail.com>
2018-06-27 21:05:22 -0500

    intentando validar datos

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 /* Holiii acá va tu código también */
+ function inicio(){
+    const done = 0;
+    const userEmail = document.login.userEmail.value;
+    userEmail = userEmail.toLowerCase();
+    const password = document.login.password.value;
+    password = password.toLowerCase();
+    if (userEmail == "usuario" && password == "contraseña") {
+        window.location = "Home.html"; done = 1;
+    }if (done == 0) {
+        window.location = "Login.html";        
+    }
+
+}
